5491cc05aea73a3a2bbc6cf0fc1e40a16ba1f699
memorex-96 <memorex@DESKTOP-4T0AHP0>
2025-03-26 01:43:24 -0400

    cmake stuff

---
+++ build/CMakeFiles/AIProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AIProject.dir/home/memorex/AIProject/libraries/math/MatrixMult.c.o"
+  "CMakeFiles/AIProject.dir/home/memorex/AIProject/libraries/math/MatrixMult.c.o.d"
+  "libAIProject.pdb"
+  "libAIProject.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AIProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -2,6 +2,10 @@ import ctypes
 import platform 
 
 if platform.system() == "Linux" or "linux2": 
-    print("On Linux") 
+    print("On Linux")
+    c_libs = ctypes.CDLL("") 
 elif platform.system() == "Windows": 
-    print("On Windows") 
+    print("On Windows")
+    c_libs = ctypes.CDLL() 
+
+
