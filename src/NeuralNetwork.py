6b64265d088b2dee8cc108a5d73d39890eb56d5a
memorex-96 <memorex@DESKTOP-4T0AHP0>
2025-03-28 10:46:47 -0400

    Fixed .so link

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -3,9 +3,9 @@ import platform
 
 if platform.system() == "Linux" or "linux2": 
     print("On Linux")
-    c_libs = ctypes.CDLL("") 
+    c_libs = ctypes.CDLL("../build/libAIProject.so") 
 elif platform.system() == "Windows": 
     print("On Windows")
-    c_libs = ctypes.CDLL("../build/libAIProject.so") 
+    c_libs = ctypes.CDLL("") 
 
 
